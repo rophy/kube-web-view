98f87f53a346644b0c266c5e812da465f5bb8d80
Henning Jacobs <henning@zalando.de>
2021-02-05 12:24:40 +0100

    v21.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = "Kubernetes Web View"
 copyright = "2019, Henning Jacobs"
 author = "Henning Jacobs"
-version = "21.1.0"
+version = "21.2.0"
 
 
 # -- General configuration ---------------------------------------------------
