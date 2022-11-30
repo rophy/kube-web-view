011c784bbda4557e251c96cf39549d8591da8d2e
Henning Jacobs <henning@zalando.de>
2022-11-30 18:23:07 +0100

    v22.11.0

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 project = "Kubernetes Web View"
 copyright = "2019, Henning Jacobs"
 author = "Henning Jacobs"
-version = "22.9.0"
+version = "22.11.0"
 
 
 # -- General configuration ---------------------------------------------------
