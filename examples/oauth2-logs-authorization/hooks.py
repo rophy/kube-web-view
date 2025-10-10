3150f9f9fa9fc98fade9e0611f03f6103780bff6
Rophy Tsai <rophy@users.noreply.github.com>
2025-10-10 06:33:16 +0000

    fix: syntax error in logs-authorization example

--- examples/oauth2-logs-authorization/hooks.py
+++ examples/oauth2-logs-authorization/hooks.py
@@ -6,13 +6,15 @@ To be used with --logs-authorized-hook option
 """
 import logging
 
+
 async def logs_authorized(cluster, namespace: str, resource, session) -> bool:
     # Assume your oauth2_authorized_hook has stored user info in session
     email = session.get("email", "unknown")
 
-    logger.info(
+    logging.info(
         f"Checking log access for user {email} to {resource.kind}/{resource.name} in {namespace}"
     )
 
-    if namespace == "kube-system" return False
+    if namespace == "kube-system":
+        return False
     return True
